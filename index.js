124af0a66da9eb7485ee4f7e7eacb489138bb70f
vaca22 <wsmslgh@gmail.com>
2022-04-07 17:09:12 +0800

    hjkfgdfbg

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 const express = require('express');
+
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 app.use('/', express.static(__dirname));
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
     res.end()
 });
 
+app.post('/', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            avatar.mv('./uploads/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 app.get('/status', (req, res) => {
     console.log("fuck2")
