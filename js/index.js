9b683c77c5d28997858d5c257aa0a7b060c2d89f
vaca22 <wsmslgh@gmail.com>
2022-04-08 18:14:56 +0800

    hjkfgdfbgffgdfhfdgsdffgcfdgdfgdfggddsffgvssdafasdrfgsdsffgdfdfggffdg

--- js/index.js
+++ js/index.js
@@ -2,16 +2,27 @@ document.addEventListener('DOMContentLoaded', function (event) {
     var baseHost = document.location.origin
     var streamUrl = baseHost + ':81'
 
+    const bau = document.getElementById('bau')
+    const button_save = document.getElementById('save')
+    bau.onchange = () => {
+        console.log(bau.value)
+    }
 
-    const framesize = document.getElementById('bau')
+    const wifi_name = document.getElementById('wifi_name')
+    const wifi_password = document.getElementById('wifi_password')
+    const ble_name = document.getElementById('ble_name')
+    const is_connect_router = document.getElementById('is_connect_router')
+    const router_name = document.getElementById('router_name')
+    const router_password = document.getElementById('router_password')
+    const inner_ip = document.getElementById('inner_ip')
+    const tcp_port = document.getElementById('tcp_port')
+    const udp_port = document.getElementById('udp_port')
 
-    framesize.onchange = () => {
-
-        console.log(framesize.value)
+    button_save.onclick=()=>{
+        console.log("good"+wifi_name.value)
 
     }
 
-    console.log(framesize.value)
 
 })
 
